ce07b9defe423ccd02ad7bf64c99b97d83b2f5fb
gogogoghost <jaxyang39@gmail.com>
2025-03-04 15:58:39 +0800

    删除单独的测试代码

--- test.sh
+++ test.sh
@@ -6,4 +6,4 @@ mkdir -p build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Debug ..
 make -j8
-./testMain
+./decoder /home/ghost/文档/xwechat_files/wxid_s88qoerexckx22_851a/msg/file/2025-03/test_2.0.0_audio.data-s0 out.wav
